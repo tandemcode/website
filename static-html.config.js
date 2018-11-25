cede52d8232fec2dfd4d4d5c07a5ac9f9bd4d699
Craig Condon <craig.j.condon@gmail.com>
2018-11-25 13:08:24 +0200

    update text

--- static-html.config.js
+++ static-html.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	project: JSON.parse(fs.readFileSync(__dirname + "/app.tdproject", "utf8")),
 	pages: [
 		{
+			title: "Build web components visually",
 			fileName: "index.html",
 			component: {
 				name: "Website",
